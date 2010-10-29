4ac3c23c483b182111b5a89ec64dbd75a2b8632d
Travis Baker <travis.baker@tribewire.com>
2010-10-29 16:08:37 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRest::Application.config.secret_token = '71ad8efadcc1da1be3621886f9e00b11a31ad10ba6a60a0f49f936e2181d3167daa9776cb5f3ec7b0c67d196562940cecc84a237b392a79ad66bf6bb5d48e898'
